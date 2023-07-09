dd80ae8e664e45d48fb82ee7cb2b65685bb54ecd
jpa203 <jeanpaulazzopardi@gmail.com>
2023-07-09 14:16:12 -0400

    new

--- main_code/cards/models.py
+++ main_code/cards/models.py
@@ -1,9 +1,13 @@
 # cards/models.py
 
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 class CardSet(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100) # for large amounts of text, yuse TextField
+    date_created = models.DateTimeField(default = timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE) # if the user is deleted, so is the quiz set
 
 class Card(models.Model): # database model ORM
     #quiz_set = models.ForeignKey(QuizSet, on_delete=models.CASCADE, null=True, blank=True)
