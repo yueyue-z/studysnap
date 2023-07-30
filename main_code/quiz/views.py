b66f73a9e2adf03e2f514c57af2e2ddd941dc920
ad5960 <ayushdixitlko@gmail.com>
2023-07-30 18:49:37 -0400

    Only Quizzes that have cards in them are visible

--- main_code/quiz/views.py
+++ main_code/quiz/views.py
@@ -4,15 +4,16 @@ from django.shortcuts import render
 from django.views import View
 from cards.models import CardSet
 from django.views.generic import  DetailView
+from django.db.models import Count
 
 class QuizHomeView(View):
     def get(self, request):
         query = request.GET.get('q')
 
         if query is not None:
-            cardsets = CardSet.objects.filter(name__icontains=query)
+            cardsets = CardSet.objects.annotate(num_cards=Count('card')).filter(name__icontains=query, num_cards__gt=0)
         else:
-            cardsets = CardSet.objects.all()
+            cardsets = CardSet.objects.annotate(num_cards=Count('card')).filter(num_cards__gt=0)
 
         return render(request, 'quiz/home.html', {'cardsets': cardsets})
 
