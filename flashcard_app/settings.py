e8952373ded68bb0874c89399bbd09ed8ab8d460
jpa203 <jeanpaulazzopardi@gmail.com>
2023-08-12 08:21:06 -0400

    updated procfile/requirements

--- flashcard_app/settings.py
+++ flashcard_app/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
-import os
+import heroku 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-v_p7e2kbk@4d&qj+ebjh&=m&x-fu)xa&e%mtl+4x!uk*a4)%ny
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['studysnap-django-env.eba-rgstui6w.us-east-1.elasticbeanstalk.com','127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -130,7 +130,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
